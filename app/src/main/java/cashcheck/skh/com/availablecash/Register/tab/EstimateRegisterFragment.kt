e10b739f9be1be333b81bffa506113a10c87379e
seogki <seogkihong333@gmail.com>
2018-08-06 18:22:58 +0900

    대략 추가

--- app/src/main/java/cashcheck/skh/com/availablecash/Register/tab/EstimateRegisterFragment.kt
+++ app/src/main/java/cashcheck/skh/com/availablecash/Register/tab/EstimateRegisterFragment.kt
@@ -7,6 +7,7 @@ import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AlertDialog
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,6 +15,8 @@ import cashcheck.skh.com.availablecash.Base.BaseFragment
 import cashcheck.skh.com.availablecash.Chart.ChartMainActivity
 import cashcheck.skh.com.availablecash.R
 import cashcheck.skh.com.availablecash.Util.Const
+import cashcheck.skh.com.availablecash.Util.DLog
+import cashcheck.skh.com.availablecash.Util.UtilMethod
 import cashcheck.skh.com.availablecash.databinding.FragmentEstimateRegisterBinding
 
 
@@ -67,6 +70,35 @@ class EstimateRegisterFragment : BaseFragment(), View.OnClickListener {
         } catch (e: Exception) {
             alertDialog(context!!, "다시 시도해주시기 바랍니다.")
         }
+    }
+    override fun onResume() {
+        super.onResume()
+        if (activity != null) {
+            val pref = activity!!.getSharedPreferences(Const.PreferenceTitle, MODE_PRIVATE)
+            val trans = pref.getInt(Const.EstimateTransport, 0)
+            val transDay = pref.getInt(Const.EstimateTransportDay, 0)
+            val food = pref.getInt(Const.EstimateFood, 0)
+            val foodDay = pref.getInt(Const.EstimateFoodDay, 0)
+            val electric = pref.getInt(Const.EstimateElectric, 0)
+            val phone = pref.getInt(Const.EstimatePhone, 0)
+            val house = pref.getInt(Const.EstimateHouse, 0)
+            val adjust = pref.getInt(Const.EstimateAdjust, 0)
+            val etc = pref.getInt(Const.EstimateEtc, 0)
+
+            val result = (trans * transDay) + (food * foodDay) + electric + phone + house + adjust + etc
+            val data = UtilMethod.currencyFormat(result.toString())
+            DLog.e("trans $trans + $transDay + $food + $foodDay + $result + $data")
+            binding.estimateFragEditTransport.text = Editable.Factory.getInstance().newEditable(trans.toString())
+            binding.estimateFragEditTransportDay.text = Editable.Factory.getInstance().newEditable(transDay.toString())
+            binding.estimateFragEditFood.text = Editable.Factory.getInstance().newEditable(food.toString())
+            binding.estimateFragEditFoodDay.text = Editable.Factory.getInstance().newEditable(foodDay.toString())
+            binding.estimateFragEditElectric.text = Editable.Factory.getInstance().newEditable(electric.toString())
+            binding.estimateFragEditPhonebill.text = Editable.Factory.getInstance().newEditable(phone.toString())
+            binding.estimateFragEditHouse.text = Editable.Factory.getInstance().newEditable(house.toString())
+            binding.estimateFragEditAdjust.text = Editable.Factory.getInstance().newEditable(adjust.toString())
+            binding.estimateFragEditEtc.text = Editable.Factory.getInstance().newEditable(etc.toString())
+        }
+
     }
 
 }// Required empty public constructor
